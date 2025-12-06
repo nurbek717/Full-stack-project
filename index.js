a2812c8e972ee99d0a539863a655546031f4b942
NurbekTukhtamishev <79134880+nurbek717@users.noreply.github.com>
2025-12-06 13:42:20 +0500

    get.post .delete.put metod

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const exprees = require('express')
 const app = exprees() //Espressni alohida uzgaruvchida chaqrib olayapti
+
+app.use(exprees.json())
+
+app.get('/', (req, res ) => {
+    res.status(200).json({message: "Hello Nurbek!"})
+})
+app.post('/' , (req, res) => {
+    const {firstname, lastname} = req.body
+    const massege = `His full name - ${firstname} ${lastname}`
+    res.send(massege)
+})
+app.delete("/:id", (req, res) => {
+     const {id} = req.params
+    res.send(id)
+})
+app.put("/:id" , (req, res) => {
+    const {id} = req.params
+    const body = req.body
+    res.json({id, body})
+})
+
+
+
+
 const PORT = 8080
 
 app.listen(PORT, () => console.log(`Server shu portda ishlayapti - http://localhost:${PORT}`))
